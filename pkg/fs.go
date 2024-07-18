cf578bb5a62688d4a30cbf245c24bf33e2422742
FelipeMCassiano <felipemcassiano@gmail.com>
2024-07-18 19:07:43 -0300

    feat: walkdir

--- pkg/fs.go
+++ pkg/fs.go
@@ -1,8 +1,29 @@
 package pkg
 
-import "os"
+import (
+	"context"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
 
-// TODO: waldir and remove local repo
+func WalkThroughDir(path string, fn func(context.Context, string) error) error {
+	err := filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			if err := fn(context.Background(), path); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func RemoveLocalRepo(path string) {
 	os.RemoveAll(path)
