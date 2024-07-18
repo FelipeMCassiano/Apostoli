5e33d3983c387fbcbf5ed05065e7c12d79358efc
FelipeMCassiano <felipemcassiano@gmail.com>
2024-07-18 19:07:56 -0300

    feat: upload to s3

--- pkg/s3.go
+++ pkg/s3.go
@@ -1,3 +1,24 @@
 package pkg
 
-// TODO: upload  and download from s3
+import (
+	"context"
+	"os"
+
+	"github.com/FelipeMCassiano/Apostoli/cg"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+// TODO: download from s3
+
+func UploadFile(ctx context.Context, key string) error {
+	_, err := cg.S3Client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(os.Getenv("AWS_BUCKET")),
+		Key:    aws.String(key),
+		Body:   os.Stdin,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
